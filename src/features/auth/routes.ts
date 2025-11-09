99f0e156a65c8f2e5367d120b95e6ab63cb5f69f
Juan Frausto <juan.frausto@govos.com>
2025-11-09 00:12:18 -0600

    endpoint route

--- src/features/auth/routes.ts
+++ src/features/auth/routes.ts
@@ -7,5 +7,6 @@ const ctrl = container.resolve(AuthController);
 const router = Router();
 router.post('/register', ctrl.register);
 router.post('/login', ctrl.login);
+router.get('/user/:id', ctrl.getUser);
 
 export default router;
